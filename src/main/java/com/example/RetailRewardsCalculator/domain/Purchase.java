988caa8a137298a3caeca4e6cc7dc51826f3735c
Mounika <mounikaujava@gmail.com>
2022-07-16 02:29:15 +0530

    added Purchase Model

---
+++ src/main/java/com/example/RetailRewardsCalculator/domain/Purchase.java
@@ -0,0 +1,33 @@
+package com.example.RetailRewardsCalculator.domain;
+
+import io.micrometer.core.lang.Nullable;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.PastOrPresent;
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Purchase {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @NotEmpty
+    private String customerName;
+
+    @Min(value = 1)
+    private double spendValue;
+
+    @PastOrPresent
+    private LocalDate purchaseDate;
+}
