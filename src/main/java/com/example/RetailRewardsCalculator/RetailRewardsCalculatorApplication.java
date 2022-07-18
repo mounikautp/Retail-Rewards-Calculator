a05c5f91a985080316f8043bb17b42b36406e3a9
Mounika <mounikaujava@gmail.com>
2022-07-18 11:37:33 +0530

    Data Build logic applied ....Data Builder logic is applied

--- src/main/java/com/example/RetailRewardsCalculator/RetailRewardsCalculatorApplication.java
+++ src/main/java/com/example/RetailRewardsCalculator/RetailRewardsCalculatorApplication.java
@@ -1,13 +1,33 @@
 package com.example.RetailRewardsCalculator;
 
+import com.example.RetailRewardsCalculator.domain.Purchase;
+import com.example.RetailRewardsCalculator.repository.PurchaseRepository;
+import com.example.RetailRewardsCalculator.service.PurchaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class RetailRewardsCalculatorApplication {
+import java.util.Calendar;
 
+@SpringBootApplication
+public class RetailRewardsCalculatorApplication implements CommandLineRunner {
+
+	@Autowired
+	private PurchaseService purchaseService;
 	public static void main(String[] args) {
 		SpringApplication.run(RetailRewardsCalculatorApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		for(int i=0; i < 5; i++){
+			Purchase purchase=new Purchase();
+			purchase.setCustomerId(1);
+			purchase.setCustomerName("Sample Customer");
+			purchase.setSpendValue(i+98);
+			purchase.setPurchaseDate(Calendar.getInstance().getTime());
+			purchaseService.savePurchase(purchase);
+		}
+	}
 }
